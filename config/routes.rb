a542d08b79631cbed4710489c64e1fd5be7ab8b0
alihass3 <hassan.ali@mail.utoronto.ca>
2018-10-21 11:13:14 -0400

    created routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   post 'pictures' => 'pictures#create'
 
   get 'pictures/new' => 'pictures#new'
-   
+
   get 'pictures/:id' => 'pictures#show'
+
+  root 'pictures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
